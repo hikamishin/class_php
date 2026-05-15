d7cbf51228990afed7276e2ddcabf96f0add6f7a
hikamishin <251613116+hikamishin@users.noreply.github.com>
2026-05-15 13:46:55 +0800

    fileProcess

--- fileProcess.php
+++ fileProcess.php
@@ -5,4 +5,12 @@ echo "暫存檔名: ".$_FILES["uFile"]["tmp_name"]."<br/>";
 echo "檔案尺寸: ".$_FILES["uFile"]["size"]."<br/>";
 echo "檔案種類: ".$_FILES["uFile"]["type"]."<hr/>";
 
+if ( copy($_FILES["uFile"]["tmp_name"],
+          $_FILES["uFile"]["name"])) {
+   echo "檔案上傳成功<br/>";
+   unlink($_FILES["uFile"]["tmp_name"]);
+}
+else
+   echo "檔案上傳失敗<br/>";
+
 ?>
