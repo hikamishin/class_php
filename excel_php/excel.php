fbea28ad451a831e8e26b656fb155c0abb4f0163
hikamishin <251613116+hikamishin@users.noreply.github.com>
2026-05-22 13:54:19 +0800

    1

--- excel_php/excel.php
+++ excel_php/excel.php
@@ -8,6 +8,28 @@ header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Content-type: application/vnd.ms-excel');
 header('Content-Disposition: inline; filename="applicant.xls";');
 header('Content-Transfer-Encoding: binary');
+//STEP 1
+$link =@mysqli_connect( 
+            'localhost',  // MySQL主機名稱 
+            'root',       // 使用者名稱 
+            '',  // 密碼
+            'school');  // 預設使用的資料庫名稱
+
+//STEP2
+$sql ="SELECT * FROM student";
+//STEP3
+$result=mysqli_query($link, $sql);
+
+echo "<table border='1'>";
+while( $row = mysqli_fetch_assoc($result) ){
+    echo "<tr>";
+   echo "<td>".$row["name"]."</td><td>".$row["dept"]."</td><td>".$row["city"]."</td><td><a href='del.php?sNo=".$row['No']."'>刪除</a></td><td><a href='update.php?sNo=".$row['No']."'>更新</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+//STEP5
+mysqli_close($link);
 ?>
 </head>
 <body>
